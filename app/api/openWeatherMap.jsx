f3a6f1912a41b2b04f22bb3b3f08aaebe6662abb
victor.gonzalez <victor.gonzalez@accedo.tv>
2016-10-27 20:04:37 +0200

    Prep repo for Heroru

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,6 +1,6 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'https://api.openweathermap.org/data/2.5/weather?appid=a28ff5c0cf881230567fb5ac04e28182&units=metric';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=a28ff5c0cf881230567fb5ac04e28182&units=metric';
 
 module.exports = {
     getTemp: function (location) {
