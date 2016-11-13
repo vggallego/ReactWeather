de291e680371e1ce6109c70a260835f16c66eda2
victor.gonzalez <vggallego@gmail.com>
2016-11-13 21:11:59 +0100

    Add grid about links

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -10,7 +10,16 @@ var React = require('react');
 //Only if we use render
 var About = (props) => {
     return (
-        <h3>About Component!</h3>
+        <div>
+            <h1 className="text-center">About Component!</h1>
+            <p>This is the weather application build on React. </p>
+            <p> Here are some of the tool I used:</p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react"> React framework</a>
+                </li>
+            </ul>
+        </div>
     );
 };
 
